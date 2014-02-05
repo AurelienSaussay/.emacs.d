ddb92b17638591f82879bb355eb07de4c3ba3726
othellogit <aurelien.saussay@gmail.com>
2014-02-05 15:25:16 +0100

    Added function toggle-frame-split

--- init.el
+++ init.el
@@ -340,3 +340,17 @@ searches all buffers."
 
 (add-hook 'python-mode-hook
 	  (lambda () (local-set-key (kbd "C-c t") #'run-python-spec)))
+
+
+;; From http://www.emacswiki.org/emacs/ToggleWindowSplit
+(defun toggle-frame-split ()
+  "If the frame is split vertically, split it horizontally or vice versa.
+Assumes that the frame is only split into two."
+  (interactive)
+  (unless (= (length (window-list)) 2) (error "Can only toggle a frame split in two"))
+  (let ((split-vertically-p (window-combined-p)))
+    (delete-window) ; closes current window
+    (if split-vertically-p
+        (split-window-horizontally)
+      (split-window-vertically)) ; gives us a split with the other window twice
+    (switch-to-buffer nil))) ; restore the original window in this part of the frame
