dd1ad2a54a0a64ce42c335e777d8e3a5ca365f47
Aurelien Saussay <aurelien.saussay@gmail.com>
2014-07-11 12:02:43 +0200

    Finished installing nimrod-mode dependencies

---
+++ elpa/popup-20140207.1702/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21439 46158
+;;;;;;  603000 0))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
